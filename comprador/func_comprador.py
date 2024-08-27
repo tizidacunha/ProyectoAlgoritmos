00a0c3bb7a95637f9b54113c481078371663551e
Tiziano da Cunha <tizidac2004@gmail.com>
2024-08-27 14:16:51 -0300

    Agrego funciones del comprador

---
+++ comprador/func_comprador.py
@@ -0,0 +1,31 @@
+#FUNCIONES DEL COMPRADOR
+
+def comprar_productos():
+    #Esta funcion permitira al comprador comprar un producto
+    pass
+
+def ver_productos():
+    #Esta funcion permitira que el comprador vea la lista con los productos disponibles
+    pass
+
+def detalles_productos():
+    #Esta funcion permitira que el comprador vea unos detalles breves del producto que seleccione
+    pass
+
+def agregar_producto_carrito():
+    #Esta funcion permitira que el comprador agregue productos al carrito
+    #Estos pueden ser modificados dentro de este
+    pass
+
+def iniciar_sesion():
+    #Esta funcion permitira que el comprador inicie sesion para que guarden los productos en el carrito, haya una recomendacion de productos ya comprados
+    pass
+
+def pago():
+    #Esta funcion permitira que el comprador pague al finalizar la compra (efectivo o otro metodo de pago)
+    pass
+
+
+def historial_compras():
+    #Esta funcion permitira que el comprador tenga un registro de sus compras y las pueda volver a repetir
+    pass
---
+++ main.py
@@ -0,0 +1,5 @@
+#PROGRAMA PRINCIAL
+
+#Aca se importaran todas las funciones del comprador y del vendedor
+
+from comprador.func_comprador import comprar_productos, ver_productos, detalles_productos, agregar_producto_carrito, iniciar_sesion, pago, historial_compras
