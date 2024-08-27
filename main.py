c569660247d15026a690ca6fba074c7a4727e103
[Mateo Fares] <[faresmateo2005@gmail.com]>
2024-08-27 15:00:25 -0300

    agrego funciones del vendedor al main

--- main.py
+++ main.py
@@ -3,3 +3,4 @@
 #Aca se importaran todas las funciones del comprador y del vendedor
 
 from comprador.func_comprador import comprar_productos, ver_productos, detalles_productos, agregar_producto_carrito, iniciar_sesion, pago, historial_compras
+from vendedor.func_vendedor import Gestion_de_productos, Gestion_de_pedidos, estadisticas
