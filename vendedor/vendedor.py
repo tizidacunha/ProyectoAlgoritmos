690ca0b295441074daa2aa6fade8e0a03c24ee11
[Mateo Fares] <[faresmateo2005@gmail.com]>
2024-08-27 14:44:00 -0300

    agrego funciones del vendedor

--- vendedor/vendedor.py
+++ vendedor/vendedor.py
@@ -1,1 +1,18 @@
 #FUNCIONES DEL VENDEDOR DEL E-COMMERCE
+
+def Gestion_de_productos():
+    """En esta funcion se podra editar, eliminar, agregar y ver productos"""
+    pass
+
+
+    
+def Gestion_de_pedidos():
+    """En esta funcion se podra gestionar los pedidos que lleguen del comprador"""
+    pass
+
+
+
+def estadisticas():
+    """En esta funcion se podran ver las estadisticas de gastos/ganancias, y tambien lo recaudado en el banco"""
+    pass
+ 
