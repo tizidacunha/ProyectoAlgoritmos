c84b5066e1cb9ed0bb59e0a8a39aecba7a674ad7
[Mateo Fares] <[faresmateo2005@gmail.com]>
2024-09-02 15:04:07 -0300

    Agrego funcion Agregar_productos:

--- vendedor/func_vendedor.py
+++ vendedor/func_vendedor.py
@@ -1,10 +1,37 @@
 #FUNCIONES DEL VENDEDOR DEL E-COMMERCE
 
-def Gestion_de_productos():
-    """En esta funcion se podra editar, eliminar, agregar y ver productos"""
+def Agregar_productos(lista):
+    """En esta funcion se podra agregar productos"""
+    
+    nombre = input("Ingresa el nombre del producto, si desea terminar ingrese -1: ")
+    
+    while nombre != "-1":
+        producto_existente = False
+        for i in lista:
+            if nombre == i[0]:
+                print("El producto ya existe")
+                producto_existente = True
+
+        if producto_existente == False:
+                
+            cantidad = int(input("Ingresa la cantidad del producto: "))
+            while cantidad < 1:
+                print("Ingrese una cantidad valida")
+            precio = float(input("Ingresa el precio del producto: "))
+            while precio <= 0:
+                print("Ingrese un precio valido")
+            lista.append([nombre, cantidad, precio])
+        
+        nombre = input("Ingresa el nombre del producto, si desea terminar ingrese -1: ")
+    return lista
+
+def Eliminar_productos():
+    """En esta funcion se podra eliminar productos"""
     pass
 
-
+def Editar_productos():
+    """En esta funcion se podra editar productos"""
+    pass
     
 def Gestion_de_pedidos():
     """En esta funcion se podra gestionar los pedidos que lleguen del comprador"""
